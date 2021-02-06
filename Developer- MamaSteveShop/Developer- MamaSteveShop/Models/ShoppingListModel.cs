b25d090a3e6443862e4b2a3968d6ac1f97746aa2
abuuhlasoi@gmail.com <abuuhlasoi@gmail.com>
2021-02-06 13:29:12 +0300

    Abraham- Changes

---
+++ Developer- MamaSteveShop/Developer- MamaSteveShop/Models/ShoppingListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Developer__MamaSteveShop.Models
+{
+    public class ShoppingListModel
+    {
+        public string Type { get; set; }
+        public Items items { get; set; }
+    }
+    public partial class Items 
+    {
+        public string Item { get; set; }
+        public decimal Price { get; set; } 
+    }
+}
